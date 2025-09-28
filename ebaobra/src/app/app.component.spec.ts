5d9086360f8944344782415501960e18362db93f
Wellington M. F. Zambelli <wellington.zambelli@otus7.com>
2025-09-28 12:08:35 -0300

    add

--- ebaobra/src/app/app.component.spec.ts
+++ ebaobra/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'obraobra' title`, () => {
+  it(`should have the 'ebaobra' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('obraobra');
+    expect(app.title).toEqual('ebaobra');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, obraobra');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, ebaobra');
   });
 });
